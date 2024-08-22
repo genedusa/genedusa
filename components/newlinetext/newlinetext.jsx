beface394d0b599ebc1469dbf226c62ca40e3d38
xxmistacruzxx <dacruz04@optonline.net>
2024-08-22 16:46:47 -0400

    Fixed key issue in newlinetext.jsx. Audited packages.

--- components/newlinetext/newlinetext.jsx
+++ components/newlinetext/newlinetext.jsx
@@ -1,16 +1,14 @@
+import { Fragment } from "react";
+
 export default function NewLineText({ text }) {
   let newtext = text.split("\n");
-  return (
-    <>
-      {newtext.map((t, index) => {
-        if (index === newtext.length - 1) return t;
-        return (
-          <>
-            {t}
-            <br />
-          </>
-        );
-      })}
-    </>
-  );
+  return newtext.map((t, index) => {
+    if (index === newtext.length - 1) return t;
+    return (
+      <Fragment key={index}>
+        {t}
+        <br />
+      </Fragment>
+    );
+  });
 }
